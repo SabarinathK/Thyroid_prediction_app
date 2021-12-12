32951c2e5677140d2281a0a5152dd1b3f330e388
SabarinathK <71809455+SabarinathK@users.noreply.github.com>
2021-12-12 18:28:19 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,6 +10,24 @@ application = Flask(__name__)
 def home():  
     return render_template('index.html')
 
+@application.route("/prediction", methods=['POST'] )
+def prediction():
+    if request.method == "POST":
+        TSH =(request.form["TSH"])
+        FTI =(request.form["FTI"])
+        TT4 =(request.form["TT4"])
+        T3 =(request.form["T3"])
+        query_hypothyroid =(request.form["query_hypothyroid"])
+        on_thyroxine =(request.form["on_thyroxine"])
+        sex =(request.form["sex"])
+        pregnant =(request.form["pregnant"])
+        psych =(request.form["psych"])
+        model=joblib.load("Thyroid_Disease_Detection.pkl")
+        arr=np.array([[TSH, FTI ,TT4, T3,query_hypothyroid,on_thyroxine,sex,pregnant,psych]])
+        prediction=model.predict(arr)
+        
+    return render_template('after.html',data=prediction)
+
 
 
 
